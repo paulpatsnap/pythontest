49546087929e44163a10b1b17e9ccda86f922455
paulphua <phuayinghui@patsnap.com>
2025-06-03 10:42:12 +0800

    pytest test

--- test_app.py
+++ test_app.py
@@ -2,6 +2,7 @@
 from app import app
 
 def test_home():
-    with app.test_client() as client:
-        response = client.get("/")
-        assert response.data == b"Hello, World from Jenkins!"
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Hello" in response.data
